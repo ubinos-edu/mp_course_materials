09c5d9447a6cf6c227d5916cd89a4bfd3070af17
Sungho Park <siminsungho@gmail.com>
2022-05-04 06:37:28 +0900

    add examples for chapter 7 - 5.

---
+++ app/mp_course_ex0712_multiple_load_store_fpu_netduinoplus2_baremetal_qemu.cmake
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2022 Sung Ho Park and CSOS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# ubinos_config_info {"name_base": "mp_course_ex0712_multiple_load_store_fpu", "build_type": "cmake_ubinos", "app": true}
+
+set_cache(UBINOS__BSP__USE_QEMU TRUE  BOOL)
+
+include(${PROJECT_UBINOS_DIR}/config/ubinos_netduinoplus2_baremetal.cmake)
+
+####
+
+set(INCLUDE__APP TRUE)
+set(APP__NAME "mp_course_ex0712_multiple_load_store_fpu")
+
+get_filename_component(_tmp_source_dir "${CMAKE_CURRENT_LIST_DIR}/${APP__NAME}" ABSOLUTE)
+
+file(GLOB_RECURSE _tmp_sources
+    "${_tmp_source_dir}/*.c"
+    "${_tmp_source_dir}/*.cpp"
+    "${_tmp_source_dir}/*.cc"
+    "${_tmp_source_dir}/*.S")
+
+set(PROJECT_APP_SOURCES ${PROJECT_APP_SOURCES} ${_tmp_sources})
+
+add_compile_options(-Wno-error=unused-variable -Wno-error=unused-function -Wno-error=unused-const-variable -Wno-error=unused-but-set-variable)
+
